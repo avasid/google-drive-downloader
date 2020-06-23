28b818e36dcbf6cfe7fba38f3967ec6059bb8f7a
avasid <avasid@bt.iitr.ac.in>
2020-06-23 10:11:28 +0530

    Minor changes
    Exception handling, token refresh, ability to continue the session

--- google_drive_downloader.py
+++ google_drive_downloader.py
@@ -1,62 +1,55 @@
-import os
-import sys
-
 import getpass
-import re
-import requests
+import os
 import time
 
 import downloadipy
+import requests
 
 
 def get_auth_code():
-    auth_url = "{0}?client_id={1}&redirect_uri={2}&response_type=code&scope={3}".format(
-        AUTH_URI, CLIENT_ID, REDIRECT_URI, ' '.join(SCOPES))
+    auth_url = "{0}?client_id={1}&redirect_uri={2}&response_type=code&scope={3}".format(AUTH_URI, CLIENT_ID,
+                                                                                        REDIRECT_URI, ' '.join(SCOPES))
     print("Paste the link from this URL here:\n", auth_url)
     auth_code = str(getpass.getpass())
     return auth_code
 
 
-def code_to_token(token=None, auth_code=None):
+def get_token(token=None):
     if token is None:
-        refresh = False
-    elif 'refresh_token' not in token.keys():
+        token = {}
+    if 'refresh_token' not in token.keys():
         auth_code = get_auth_code()
-    else:
-        refresh = True
-    if refresh:
-        addon_dict = {"grant_type": "refresh_token",
-                      "refresh_token": token['refresh_token']}
-    else:
         addon_dict = {"grant_type": "authorization_code",
                       "scope": ' '.join(SCOPES),
                       "redirect_uri": REDIRECT_URI,
                       "content_type": "application/x-www-form-urlencoded",
                       "code": auth_code}
+    else:
+        addon_dict = {"grant_type": "refresh_token",
+                      "refresh_token": token['refresh_token']}
+
     params = {"client_id": CLIENT_ID,
-              "client_secret": CLIENT_SECRET,
-              "grant_type": "authorization_code"}
+              "client_secret": CLIENT_SECRET}
     params.update(addon_dict)
     token_req = requests.request("POST", TOKEN_URI, data=params)
     try:
         token_dict = eval(token_req.text)
         if type(token_dict) == dict:
             # Buffer time for processing
-            token_dict['expires_at'] = time.time(
-            ) + token_dict['expires_in'] - 10
-            return token_dict
+            token_dict['expires_at'] = time.time() + token_dict['expires_in'] - 10
+            token.update(token_dict)
+            return token
         else:
-            print("Token failed to convert to dict")
-            print(token_dict)
+            raise ValueError("Token failed to convert to dict: " + str(token_dict) + ", Type: " + str(type(token_dict)))
     except:
-        print("Token failed to convert")
+        print("Token error")
         print(token_req.text)
-        sys.exit()
+        raise
 
 
 def check_token(token):
     if token['expires_at'] - 10 <= time.time():  # BufferTime
-        token = code_to_token(token)
+        token = get_token(token)
     return token
 
 
@@ -76,12 +69,12 @@ def get_headers(file_id, token):
              "alt": "media"}
     req_url = get_download_url(file_id, param)
     req_response = requests.request("GET", req_url, headers=param, stream=True)
-    if req_response.status_code != 200:
+    if not req_response.ok:
         if req_response.status_code == 403 and "cannotDownloadAbusiveFile" in req_response.text:
             param['acknowledgeAbuse'] = "true"
         else:
-            print("Error:", req_response.status_code)
             print(req_response.text)
+            req_response.raise_for_status()
             return None
     return param
 
@@ -103,18 +96,17 @@ def get_children(folder_id, token, page_token=None):
         param['pageToken'] = page_token
     req_url = "https://www.googleapis.com/drive/v3/files" + param_str
     req_response = requests.request("GET", req_url, headers=param)
-    if req_response.status_code != 200:
-        print("Error", req_response.status_code)
+    if not req_response.ok:
         print(req_response.text)
-        sys.exit()
+        req_response.raise_for_status()
     try:
         results = eval(req_response.text)
         if type(results) != dict:
-            print("Response failed to convert to dict")
-            print(results)
+            raise ValueError("Response failed to convert to dict: " + str(results) + ", Type: " + str(type(results)))
     except:
-        print("Response failed to convert")
-        sys.exit()
+        print("Response failed")
+        print(req_response.text)
+        raise
     return results
 
 
@@ -122,33 +114,25 @@ def get_children_schema(folder_id, token):
     page_token = None
     while True:
         token = check_token(token)
-        try:
-            results = get_children(folder_id, token, page_token)
-            items = results.get('files', [])
-            if not items:
-                print("No files found")
-                return []
-            else:
-                item_list = []
-                for item in items:
-                    if item['mimeType'] == "application/vnd.google-apps.folder":
-                        folder_dict = {}
-                        folder_dict[(item['id'], item['name'])
-                                    ] = get_children_schema(item['id'], token)
-                        item_list.append(folder_dict)
-                        size = 'IsDir'
-                    else:
-                        size = downloadipy.Downloader.humanize_bytes(
-                            int(item['size']))
-                        item_list.append((item['id'], item['name']))
-                    print(u'{0}    [Size: {1}] ({2})'.format(
-                        item['name'], size, item['id']))
-                page_token = results.get('nextPageToken')
-                if not page_token:
-                    return item_list
-        except:
-            print("An error occured: %s" % sys.exc_info()[0])
-            break
+        results = get_children(folder_id, token, page_token)
+        items = results.get('files', [])
+        if not items:
+            print("No files found")
+            return []
+        else:
+            item_list = []
+            for item in items:
+                if item['mimeType'] == "application/vnd.google-apps.folder":
+                    folder_dict = {(item['id'], item['name']): get_children_schema(item['id'], token)}
+                    item_list.append(folder_dict)
+                    size = 'IsDir'
+                else:
+                    size = downloadipy.Downloader.humanize_bytes(int(item['size']))
+                    item_list.append((item['id'], item['name']))
+                print(u'{0}    [Size: {1}] ({2})'.format(item['name'], size, item['id']))
+            page_token = results.get('nextPageToken')
+            if not page_token:
+                return item_list
 
 
 def list_children(folder_id, token):
@@ -156,25 +140,19 @@ def list_children(folder_id, token):
     page_token = None
     while True:
         token = check_token(token)
-        try:
-            results = get_children(folder_id, token, page_token)
-            items = results.get('files', [])
-            if not items:
-                print("No files found")
-                return {}
-            else:
-                for item in items:
-                    size = downloadipy.Downloader.humanize_bytes(
-                        int(item.get('size', 0)))
-                    total_items[item['name']] = (size, item['id'])
-                    print(u'{0}    [Size: {1}] ({2})'.format(
-                        item['name'], size, item['id']))
-                page_token = results.get('nextPageToken')
-                if not page_token:
-                    return total_items
-        except:
-            print("An error occured: %s" % sys.exc_info()[0])
-            break
+        results = get_children(folder_id, token, page_token)
+        items = results.get('files', [])
+        if not items:
+            print("No files found")
+            return {}
+        else:
+            for item in items:
+                size = downloadipy.Downloader.humanize_bytes(int(item.get('size', 0)))
+                total_items[item['name']] = (size, item['id'])
+                print(u'{0}    [Size: {1}] ({2})'.format(item['name'], size, item['id']))
+            page_token = results.get('nextPageToken')
+            if not page_token:
+                return total_items
 
 
 def schema_handler(schema, cwd):
@@ -187,8 +165,7 @@ def schema_handler(schema, cwd):
             elif type(elem) == tuple:
                 param = get_headers(elem[0], token)
                 download_url = get_download_url(elem[0], param)
-                downloadipy.Downloader(
-                    download_url, cwd + elem[1], headers=param).download()
+                downloadipy.Downloader(download_url, cwd + elem[1], headers=param).download()
             else:
                 print("What is it?", elem)
     else:
@@ -197,33 +174,28 @@ def schema_handler(schema, cwd):
 
 def get_mimetype(unique_id, token):
     token = check_token(token)
+    param = {"Authorization": "Bearer " + token['access_token'],
+             "includeItemsFromAllDrives": "true",
+             "supportsAllDrives": "true",
+             "fields": "id,name,mimeType"}
+    param_str = ""
+    for k, v in param.items():
+        param_str += k + "=" + v + "&"
+    param_str = "?" + param_str[:-1]
+    req_url = "https://www.googleapis.com/drive/v3/files/" + unique_id + param_str
+    req_response = requests.request("GET", req_url, headers=param)
+    if not req_response.ok:
+        print(req_response.text)
+        req_response.raise_for_status()
     try:
-        param = {"Authorization": "Bearer " + token['access_token'],
-                 "includeItemsFromAllDrives": "true",
-                 "supportsAllDrives": "true",
-                 "fields": "id,name,mimeType"}
-        param_str = ""
-        for k, v in param.items():
-            param_str += k + "=" + v + "&"
-        param_str = "?" + param_str[:-1]
-        req_url = "https://www.googleapis.com/drive/v3/files/" + unique_id + param_str
-        req_response = requests.request("GET", req_url, headers=param)
-        if req_response.status_code != 200:
-            print("Error", req_response.status_code)
-            print(req_response.text)
-            sys.exit()
-        try:
-            results = eval(req_response.text)
-            if type(results) != dict:
-                print("Response failed to convert to dict")
-                print(results)
-        except:
-            print("Response failed to convert")
-            sys.exit()
-        return (results['name'], results['mimeType'])
+        results = eval(req_response.text)
+        if type(results) != dict:
+            raise ValueError("Response failed to convert to dict: " + str(results) + ", Type: " + str(type(results)))
     except:
-        print("An error occured: %s" % sys.exc_info()[0])
-        sys.exit()
+        print("Response failed to convert")
+        print(req_response.text)
+        raise
+    return results['name'], results['mimeType']
 
 
 def by_id(unique_id, token, dest):
@@ -233,8 +205,7 @@ def by_id(unique_id, token, dest):
     if mime != "application/vnd.google-apps.folder":
         param = get_headers(unique_id, token)
         download_url = get_download_url(unique_id, param)
-        downloadipy.Downloader(download_url, os.path.join(
-            dest, name), headers=param).download()
+        downloadipy.Downloader(download_url, os.path.join(dest, name), headers=param).download()
     else:
         os.makedirs(os.path.join(dest, name), exist_ok=True)
         schema = get_children_schema(unique_id, token)
@@ -246,7 +217,7 @@ def by_name(folder_id, token, dest):
     if items_dict == {}:
         print("Maybe it's a file, retry with [A]ll option")
         return
-    while(True):
+    while True:
         search_string = str(input("Part of filename or whole file ID: "))
         found_files = []
         for name, (size, uid) in items_dict.items():
@@ -257,13 +228,14 @@ def by_name(folder_id, token, dest):
                 break
         if len(found_files) != 1:
             print("{} files match the criterion".format(len(found_files)))
-            if found_files != []:
+            if found_files:
                 for file in found_files:
                     print('{0}    [Size: {1}] ({2})'.format(*file))
         elif len(found_files) == 1:
             file_id = found_files[0][2]
             by_id(file_id, token, dest)
             break
+
 
 with open("creds.json", 'r') as fh:
     creds_json = fh.read()
@@ -276,24 +248,34 @@ if type(creds_dict) == dict:
     REDIRECT_URI = creds_dict['installed']['redirect_uris'][0]
     SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]  # list
 else:
-    print("Error in creds.json")
-    sys.exit()
-auth_code = get_auth_code()
-token = code_to_token(auth_code=auth_code)
+    raise ValueError("Error in creds.json")
+token = get_token()
 
-while(True):
-    item_id = str(input("Item ID: "))
-    if item_id != '':
+again = True
+item_id = None
+destination = os.getcwd()
+while again:
+    while True:
+        item_id_temp = str(input("Item ID [Default: {}]: ".format(item_id)))
+        if item_id_temp != '':
+            item_id = item_id_temp
+            break
+        elif item_id != '':
+            break
+        print("Empty ID")
+    destination_temp = str(input("Location for download [Default: {}]: ".format(destination)))
+    if destination_temp != '':
+        destination = destination_temp
+    while True:
+        choice = str(input("[A]ll | [P]art: "))
+        if choice == 'A':
+            by_id(item_id, token, destination)
+        elif choice == 'P':
+            by_name(item_id, token, destination)
+        else:
+            print("No known choice")
+            continue
         break
-    print("Empty ID")
-destination = str(input("Location for download: "))
-while True:
-    choice = str(input("[A]ll | [P]art: "))
-    if choice == 'A':
-        by_id(item_id, token, destination)
-    elif choice == 'P':
-        by_name(item_id, token, destination)
-    else:
-        print("No known choice")
-        continue
-    break
+    again_resp = str(input("Download another item? [Y|N]: "))
+    if again_resp == "N":
+        again = False
